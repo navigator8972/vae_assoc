d6d4120a6bf1eedff9b665831ad62f7946e46b78
navigator8972 <navigator8972@gmail.com>
2016-04-26 19:04:24 +0100

    Commit the change the utils.py. Also test the modified mail address

--- utils.py
+++ utils.py
@@ -77,8 +77,8 @@ def generate_images_for_chars_and_digits(data, overwrite=False, grayscale=True, 
                 fig, ax = plot_single_stroke_char_or_digit(d)
                 extent = ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
                 fig.savefig(tmp_fpath, bbox_inches=extent, dpi=100)
-           
-            if grayscale: 
+
+            if grayscale:
                 #load the image to have a grayscale file
                 image=Image.open(tmp_fpath).convert("L")
                 image.thumbnail(thumbnail_size)
@@ -128,3 +128,60 @@ def expand_traj_dim_with_derivative(data, dt=0.01):
             augmented_trajs.append(tmp_augmented_traj)
 
     return augmented_trajs
+
+import dataset as ds
+
+def extract_images(data=None, fname=None, only_digits=True, dtype=np.float32):
+    data_dict = data
+    if fname is not None:
+        #try to load from given fname
+        data_dict = cp.load(open(fname, 'rb'))
+
+    def extract_image_helper(d):
+        #flatten the image and scale them
+        return d.flatten().astype(dtype) * 1./255.
+    images = []
+    if data_dict is not None:
+        for char in sorted(data_dict.keys(), key=lambda k:k[-1]):
+            if only_digits and ord(char[-1]) > 57:
+                continue
+            else:
+                images += [extract_image_helper(d) for d in data_dict[char]]
+    return np.array(images)
+
+def extract_jnt_trajs(data=None, fname=None, only_digits=True, dtype=np.float32):
+    data_dict = data
+    if fname is not None:
+        #try to load from given fname
+        data_dict = cp.load(open(fname, 'rb'))
+
+    def extract_jnt_trajs_helper(d):
+        #flatten the image and scale them, is it necessary for joint trajectory, say within pi radians?
+        return d.flatten().astype(dtype)
+    jnt_trajs = []
+    if data_dict is not None:
+        for char in sorted(data_dict.keys()):
+            if only_digits and ord(char[-1]) > 57:
+                continue
+            else:
+                jnt_trajs += [extract_jnt_trajs_helper(d) for d in data_dict[char]]
+    return np.array(jnt_trajs)
+
+def extract_jnt_fa_parms(data=None, fname=None, only_digits=True, dtype=np.float32):
+    data_dict = data
+    if fname is not None:
+        #try to load from given fname
+        data_dict = cp.load(open(fname, 'rb'))
+
+    fa_parms = []
+    if data_dict is not None:
+        for char in sorted(data_dict.keys()):
+            if only_digits and ord(char[-1]) > 57:
+                continue
+            else:
+                fa_parms += [d for d in data_dict[char]]
+    fa_parms = np.array(fa_parms)
+    #Gaussian statistics for potential normalization
+    fa_mean = np.mean(fa_parms, axis=0)
+    fa_std = np.std(fa_parms, axis=0)
+    return fa_parms, fa_mean, fa_std
